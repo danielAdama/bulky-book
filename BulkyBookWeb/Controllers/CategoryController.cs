66b6b59931d204e447ecfb8cfeec1a8fc3b7fc5f
danielAdama <adamadaniel321@gmail.com>
2022-11-05 19:08:13 -0700

    Queried the Category table as a list

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -1,11 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BulkyBookWeb.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyBookWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _dbContext;
+        public CategoryController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         public IActionResult Index()
         {
+            var objCategoryList = _dbContext.Categories.ToList();
             return View();
         }
     }
