20b5198a0ccfb41ccb75b65ecdeb8667db2ffe4c
danielAdama <adamadaniel321@gmail.com>
2022-11-07 20:07:38 +0100

    Modifications

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -23,6 +23,7 @@ namespace BulkyBookWeb.Controllers
         {
             return View();
         }
+        // Create
         [HttpPost]
         public IActionResult Create(Category book)
         {
@@ -37,9 +38,70 @@ namespace BulkyBookWeb.Controllers
             {
                 _dbContext.Categories.Add(book);
                 _dbContext.SaveChanges();
+                TempData["Success"] = "Category created successfully";
                 return RedirectToAction("Index");
             }
             return View(book);
+        }
+        // Edit
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _dbContext.Categories.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        // Edit to Update
+        [HttpPost]
+        public IActionResult Edit(Category book)
+        {
+            if (book.Name == book.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _dbContext.Categories.Update(book);
+                _dbContext.SaveChanges();
+                TempData["Success"] = "Category updated successfully";
+                return RedirectToAction("Index");
+            }
+            return View(book);
+        }
+        // Detlete
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var categoryFromDb = _dbContext.Categories.Find(id);
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(categoryFromDb);
+        }
+        [HttpPost]
+        public IActionResult DeletePOST(int? id)
+        {
+            var book = _dbContext.Categories.Find(id);
+            if (book == null)
+            {
+                return NotFound();
+            }
+            _dbContext.Categories.Remove(book);
+            _dbContext.SaveChanges();
+            TempData["Success"] = "Category deleted successfully";
+            return RedirectToAction("Index");
         }
     }
 }
