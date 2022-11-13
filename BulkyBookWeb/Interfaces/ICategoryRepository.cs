058a476b0867fcf261860d47280981c483205ed2
danielAdama <adamadaniel321@gmail.com>
2022-11-13 18:42:12 +0100

    Refactored code and added Dependency Injection + Repository to aid clean architecture

---
+++ BulkyBookWeb/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,14 @@
+﻿using BulkyBookWeb.Models;
+
+namespace BulkyBookWeb.Interfaces
+{
+	public interface ICategoryRepository
+	{
+		Task<List<Category>> GetAllCategoriesAsync();
+		Task<Category> GetIdAsync(long id);
+		bool Add(Category category);
+		bool Update(Category category);
+		bool Delete(Category category);
+		bool Save();
+	}
+}
