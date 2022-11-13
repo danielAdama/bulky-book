a2c414770c98fa30c59503090398e777502a9281
danielAdama <adamadaniel321@gmail.com>
2022-11-13 18:42:41 +0100

    Refactored code and added Dependency Injection + Repository to aid clean architecture

---
+++ BulkyBookWeb/Repository/CategoryRepository.cs
@@ -0,0 +1,51 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Interfaces;
+using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+
+namespace BulkyBookWeb.Repository
+{
+	public class CategoryRepository : ICategoryRepository
+	{
+		private readonly ApplicationDbContext _context;
+
+		public CategoryRepository(ApplicationDbContext context)
+		{
+			_context = context;
+		}
+		public bool Add(Category category)
+		{
+			_context.Add(category);
+			return Save();
+		}
+
+		public bool Delete(Category category)
+		{
+			_context.Remove(category);
+			return Save();
+		}
+
+		public async Task<List<Category>> GetAllCategoriesAsync()
+		{
+			return await _context.Categories.AsNoTracking().ToListAsync();
+		}
+
+		public async Task<Category> GetIdAsync(long id)
+		{
+			return await _context.Categories.FindAsync(id);
+		}
+
+		public bool Save()
+		{
+			var saved = _context.SaveChanges();
+			return saved > 0;
+		}
+
+		public bool Update(Category category)
+		{
+			_context.Update(category);
+			return Save();
+		}
+	}
+}
