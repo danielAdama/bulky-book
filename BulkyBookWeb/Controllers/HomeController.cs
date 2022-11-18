0d43a5c99f290c6f929255932335d594e2aa4895
danielAdama <adamadaniel321@gmail.com>
2022-11-18 12:13:09 +0100

    Returned a list of Library Books

--- BulkyBookWeb/Controllers/HomeController.cs
+++ BulkyBookWeb/Controllers/HomeController.cs
@@ -1,19 +1,29 @@
-﻿using BulkyBookWeb.Models;
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
+using System.Text;
 
 namespace BulkyBookWeb.Controllers
 {
 	public class HomeController : Controller
 	{
-		private readonly ILogger<HomeController> _logger;
+		private readonly ApplicationDbContext _context;
 
-		public HomeController(ILogger<HomeController> logger)
+		public HomeController(ApplicationDbContext context)
 		{
-			_logger = logger;
+			_context = context;
+			Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 		}
 
-		public IActionResult Index()
+		public async Task<IActionResult> Index()
+		{
+			List<Library> libraryList = await _context.Libraries.AsNoTracking().ToListAsync(); 
+			return View(libraryList);
+		}
+
+		public IActionResult ExcelUpload()
 		{
 			return View();
 		}
