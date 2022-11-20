b17b00313d1deade9f0233e87b0beb585eb31434
danielAdama <adamadaniel321@gmail.com>
2022-11-20 02:53:44 +0100

    Fixes

---
+++ BulkyBookWeb/ViewModels/DisplayBooksAndUploadFileViewModel.cs
@@ -0,0 +1,13 @@
+﻿using BulkyBookWeb.ViewModels;
+using BulkyBookWeb.Models;
+
+namespace BulkyBookWeb.ViewModels
+{
+	public class DisplayBooksAndUploadFileViewModel : ResponseViewModel
+	{
+		// This property returns the list of books (GetLibraries) in the library and also
+		// aids file uploads (File)
+		public List<Library> GetLibraries { get; set; }
+		public IFormFile File { get; set; }
+	}
+}
---
+++ BulkyBookWeb/ViewModels/ResponseViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BulkyBookWeb.ViewModels
+{
+	public class ResponseViewModel
+	{
+		public bool IsSuccess { get; set; }
+		public bool IsResponse { get; set; }
+		public string Message { get; set; }
+	}
+}
