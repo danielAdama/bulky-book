4f353aee0c4082eabd7603acc824c388e3b79c97
danielAdama <adamadaniel321@gmail.com>
2022-11-22 02:34:53 +0100

    Completed single excel upload

--- BulkyBookWeb/Controllers/HomeController.cs
+++ BulkyBookWeb/Controllers/HomeController.cs
@@ -2,10 +2,14 @@
 using BulkyBookWeb.Models;
 using BulkyBookWeb.ViewModels;
 using ExcelDataReader;
+using ExcelDataReader.Log;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Collections.Concurrent;
 using System.Data;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.IO;
 using System.Text;
 using System.Threading;
@@ -60,27 +64,102 @@ namespace BulkyBookWeb.Controllers
 					DataTable data = await GetExcelTable(path, fileVM, cancellationToken);
 					System.IO.File.Delete(path);
 
-					foreach (DataRow row in data.Rows)
+
+					using var transaction = await _context.Database.BeginTransactionAsync(cancellationToken); // Track all operations
+					try
 					{
-						SyncLog log = new SyncLog()
+						ConcurrentBag<SyncLog> syncLogBag = new ConcurrentBag<SyncLog>();
+						foreach (DataRow row in data.Rows)
 						{
-							LibSyncId = syncId,
-							UserId = row["UserId"].ToString(),
-							Name = row["Name"].ToString(),
-							DisplayOrder = Convert.ToInt32(row["DisplayOrder"]),
-							Genre = row["Genre"].ToString(),
-							ISBN = row["ISBN"].ToString(),
-							Author = row["Author"].ToString(),
-							Publisher = row["Publisher"].ToString(),
+							SyncLog syncLog = new SyncLog()
+							{
+								LibSyncId = syncId,
+								UserId = row["UserId"].ToString(),
+								Name = row["Name"].ToString(),
+								DisplayOrder = Convert.ToInt32(row["DisplayOrder"]),
+								Genre = row["Genre"].ToString(),
+								ISBN = row["ISBN"].ToString(),
+								Author = row["Author"].ToString(),
+								Publisher = row["Publisher"].ToString(),
+
+							};
+							//await _context.SyncLogs.AddAsync(libraryLog, cancellationToken);
+							//await _context.SaveChangesAsync(cancellationToken);
+							// Store syncLog as concurrentbag so we can easily compare it with the librarybag
+							syncLogBag.Add(syncLog);
+						}
+
+
+						// Compare data btw Libraries(bag) and SyncLogs table (bag), if change is detected add it to the ChangesLog table
+						List<Library> libraryList = await _context.Libraries.ToListAsync(cancellationToken);
+
+						ConcurrentBag<Library> newLibrarybag = new ConcurrentBag<Library>();
+						ConcurrentBag<Library> updatedLibrarybag = new ConcurrentBag<Library>();
+						ConcurrentBag<ChangesLog> changesLibrarybag = new ConcurrentBag<ChangesLog>();
+
+						foreach (var log in syncLogBag)
+						{
+							var userLibraryData = libraryList.FirstOrDefault(x => x.UserId?.ToLower().Equals(log.UserId?.ToLower()) == true);
+							// If the userLibraryData is false means a new book is been stored otherwise check previous books
+							// for changes, when change is detected the system should flag that row of those values that changed.
+							if (null == userLibraryData) // false
+							{
+								newLibrarybag.Add(new Library
+								{
+									UserId = log.UserId,
+									Name = log.Name,
+									DisplayOrder = log.DisplayOrder,
+									Genre = log.Genre,
+									ISBN = log.ISBN,
+									Author = log.Author,
+									Publisher = log.Publisher,
+									TimeCreated = DateTime.Now,
+									TimeUpdated = DateTime.Now,
+								});
+							}
+							else // true
+							{
+								var libraryChanges = GetLibraryChanges(userLibraryData, log, out int numChanges);
+
+								ChangesLog libChanges = new ChangesLog
+								{
+									Changes = JsonConvert.SerializeObject(libraryChanges),
+									refCode = syncRef,
+									RowAffected = numChanges,
+									SyncId = syncId,
+									TimeCreated = DateTime.Now,
+									TimeUpdated = DateTime.Now,
+									UserId = log.UserId,
+								};
+
+								changesLibrarybag.Add(libChanges);
+								updatedLibrarybag.Add(userLibraryData);
+
+							}
 
 						};
-						await _context.SyncLogs.AddAsync(log, cancellationToken);
-						await _context.SaveChangesAsync(cancellationToken);
+						await _context.SyncLogs.AddRangeAsync(syncLogBag, cancellationToken);
+						await _context.ChangesLogs.AddRangeAsync(changesLibrarybag, cancellationToken);
+
+						if (newLibrarybag.Any())
+						{
+							await _context.Libraries.AddRangeAsync(newLibrarybag, cancellationToken);
+						}
+						if (updatedLibrarybag.Any())
+						{
+							_context.Libraries.UpdateRange(updatedLibrarybag);
+						}
+						await _context.SaveChangesAsync();
+
+						await transaction.CommitAsync(cancellationToken);
+						return RedirectToAction("Index");
 					}
-
-					// Compare data btw Libraries and SyncLogs table, if change is detected add it to the ChangesLog table
-					
-
+					catch (Exception ex)
+					{
+						await transaction.RollbackAsync(cancellationToken);
+						TempData["errorMessage"] = ex.Message;
+						return RedirectToAction("Index");
+					}
 
 				}
 			}
@@ -122,6 +201,159 @@ namespace BulkyBookWeb.Controllers
 			await _context.Syncs.AddAsync(refId, cancellationToken);
 			await _context.SaveChangesAsync(cancellationToken);
 			return refId.Id;
+		}
+
+		private static bool IsNotNull([NotNullWhen(true)] object? obj) => obj != null; // Method to ignore null values
+
+		public static Dictionary<string, Dictionary<string, string>> GetLibraryChanges(Library userLibraryData, SyncLog log, out int numChanges)
+		{
+			var detectedChanges = new Dictionary<string, Dictionary<string, string>>();
+			//int numChanges = 0;
+			numChanges = 0;
+
+			if (IsNotNull(userLibraryData.Author) && IsNotNull(log.Author))
+			{
+				bool IsAuthor = !userLibraryData.Author.Equals(log.Author);
+				if (IsAuthor)
+				{
+					detectedChanges.Add("Author", new Dictionary<string, string>
+								{
+									{ "Old", userLibraryData.Author.ToString() },
+									{ "New", log.Author.ToString() }
+								});
+					//userLibraryData.Author = log.Author;
+					userLibraryData.TimeUpdated = DateTime.Now;
+					numChanges += 1;
+				}
+			}
+			//bool IsAuthor = !userLibraryData.Author.Equals(log.Author);
+			//if (IsAuthor)
+			//{
+			//	detectedChanges.Add("Author", new Dictionary<string, string>
+			//					{
+			//						{ "Old", userLibraryData.Author.ToString() },
+			//						{ "New", log.Author.ToString() }
+			//					});
+			//	//userLibraryData.Author = log.Author;
+			//	userLibraryData.TimeUpdated = DateTime.Now;
+			//	numChanges += 1;
+			//}
+
+			if (IsNotNull(userLibraryData.Name) && IsNotNull(log.Name))
+			{
+				bool IsName = !userLibraryData.Name.Equals(log.Name);
+				if (IsName)
+				{
+					detectedChanges.Add("Name", new Dictionary<string, string>
+								{
+									{ "Old", userLibraryData.Name.ToString() },
+									{ "New", log.Name.ToString() }
+								});
+					//userLibraryData.Name = log.Name;
+					userLibraryData.TimeUpdated = DateTime.Now;
+					numChanges += 1;
+				}
+			}
+
+			//bool IsName = !userLibraryData.Name.Equals(log.Name);
+			//if (IsName)
+			//{
+			//	detectedChanges.Add("Name", new Dictionary<string, string>
+			//					{
+			//						{ "Old", userLibraryData.Name.ToString() },
+			//						{ "New", log.Name.ToString() }
+			//					});
+			//	//userLibraryData.Name = log.Name;
+			//	userLibraryData.TimeUpdated = DateTime.Now;
+			//	numChanges += 1;
+			//}
+
+			bool IsDisplayOrder = !userLibraryData.DisplayOrder.Equals(log.DisplayOrder);
+			if (IsDisplayOrder)
+			{
+				detectedChanges.Add("DisplayOrder", new Dictionary<string, string>
+								{
+									{ "Old", userLibraryData.DisplayOrder.ToString() },
+									{ "New", log.DisplayOrder.ToString() }
+								});
+				//userLibraryData.DisplayOrder = log.DisplayOrder;
+				userLibraryData.TimeUpdated = DateTime.Now;
+				numChanges += 1;
+			}
+
+			if (IsNotNull(userLibraryData.Genre) && IsNotNull(log.Genre))
+			{
+				bool IsGenre = !userLibraryData.Genre.Equals(log.Genre);
+				if (IsGenre)
+				{
+					detectedChanges.Add("Genre", new Dictionary<string, string>
+								{
+									{ "Old", userLibraryData.Genre.ToString() },
+									{ "New", log.Genre.ToString() }
+								});
+					//userLibraryData.Genre = log.Genre;
+					userLibraryData.TimeUpdated = DateTime.Now;
+					numChanges += 1;
+				}
+			}
+			//bool IsGenre = !userLibraryData.Genre.Equals(log.Genre);
+			//if (IsGenre)
+			//{
+			//	detectedChanges.Add("Genre", new Dictionary<string, string>
+			//					{
+			//						{ "Old", userLibraryData.Genre.ToString() },
+			//						{ "New", log.Genre.ToString() }
+			//					});
+			//	//userLibraryData.Genre = log.Genre;
+			//	userLibraryData.TimeUpdated = DateTime.Now;
+			//	numChanges += 1;
+			//}
+
+			if (IsNotNull(userLibraryData.ISBN) && IsNotNull(log.ISBN))
+			{
+				bool IsISBN = !userLibraryData.ISBN.Equals(log.ISBN);
+				if (IsISBN)
+				{
+					detectedChanges.Add("ISBN", new Dictionary<string, string>
+								{
+									{ "Old", userLibraryData.ISBN.ToString() },
+									{ "New", log.ISBN.ToString() }
+								});
+					//userLibraryData.ISBN = log.ISBN;
+					userLibraryData.TimeUpdated = DateTime.Now;
+					numChanges += 1;
+				}
+			}
+
+			if (IsNotNull(userLibraryData.Publisher) && IsNotNull(log.Publisher))
+			{
+				bool IsPublisher = !userLibraryData.Publisher.Equals(log.Publisher);
+				if (IsPublisher)
+				{
+					detectedChanges.Add("Publisher", new Dictionary<string, string>
+								{
+									{ "Old", userLibraryData.Publisher.ToString() },
+									{ "New", log.Publisher.ToString() }
+								});
+					//userLibraryData.Publisher = log.Publisher;
+					userLibraryData.TimeUpdated = DateTime.Now;
+					numChanges += 1;
+				}
+			}
+
+			//bool IsPublisher = !userLibraryData.Publisher.Equals(log.Publisher);
+			//if (IsPublisher)
+			//{
+			//	detectedChanges.Add("Publisher", new Dictionary<string, string>
+			//					{
+			//						{ "Old", userLibraryData.Publisher.ToString() },
+			//						{ "New", log.Publisher.ToString() }
+			//					});
+			//	//userLibraryData.Publisher = log.Publisher;
+			//	userLibraryData.TimeUpdated = DateTime.Now;
+			//	numChanges += 1;
+			//}
+			return detectedChanges;
 		}
 
 		public IActionResult Privacy()
