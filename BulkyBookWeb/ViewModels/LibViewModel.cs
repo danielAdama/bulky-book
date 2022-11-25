ca2b9d6d3d9962eef9dc6b20418a6940e24dfc8f
danielAdama <adamadaniel321@gmail.com>
2022-11-25 11:30:42 +0100

    LibViewModel as datatype for csv reader

---
+++ BulkyBookWeb/ViewModels/LibViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+namespace BulkyBookWeb.ViewModels
+{
+	public class LibViewModel
+	{
+		public string? UserId { get; set; }
+		[Required]
+		public string Name { get; set; }
+		[DisplayName("Display Order")]
+		[Range(1, 100, ErrorMessage = "Display Order must be between 1 and 100 only!")]
+		public int DisplayOrder { get; set; }
+		public string? Genre { get; set; }
+		public string? ISBN { get; set; }
+		public string? Author { get; set; }
+		public string? Publisher { get; set; }
+	}
+}
